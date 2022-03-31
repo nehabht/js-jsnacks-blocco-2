4318e6d5d81f112bdd360bd14cb6fd5e9bdf1ee0
nehabht <ne.bhatti2@icloud.com>
2022-03-31 15:51:06 +0200

    add snack-2

---
+++ jsnack2/script.js
@@ -0,0 +1,14 @@
+// Snack2
+
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo.
+
+
+x = 3
+
+
+if (x % 2 === 0){
+    console.log("pari")
+} else {
+    console.log(x + 1)
+}
